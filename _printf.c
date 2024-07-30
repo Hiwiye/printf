548b8aec562120ed8b410fb1e2c04f239eead9a1
Dr. Hiwot Wondale <hiwotwondalemd@gmail.com>
2024-07-30 11:20:42 +0300

    added printf

---
+++ _printf.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: character string containing zero or more directives
+ *
+ * Return: the number of characters printed (excluding the null byte
+ * used to end output to strings)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+	char c;
+	char *str;
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				c = va_arg(args, int);
+				count += write(1, &c, 1);
+			}
+			else if (*format == 's')
+			{
+				str = va_arg(args, char *);
+				while (*str)
+					count += write(1, str++, 1);
+			}
+			else if (*format == '%')
+			{
+				count += write(1, "%", 1);
+			}
+		}
+		else
+		{
+			count += write(1, format, 1);
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
+
